d1083b183e623283b0dcc98e33e74bfc0929f8fe
João Paulo <jp20171617@gmail.com>
2024-01-23 00:01:47 -0300

    conectando por senha

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,4 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/local');
 
-module.exports = mongoose;
+const mongoose = require('mongoose');
+const connectdDB = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://JoaoPaulo:tsmbjergsen123@api-biblioteca.ceke1rz.mongodb.net/?retryWrites=true&w=majority');
+        console.log("Conectado")
+    }
+    catch (error) {
+        console.log('Não conectou' + error)
+    }
+} 
+
+
+module.exports = connectdDB;
